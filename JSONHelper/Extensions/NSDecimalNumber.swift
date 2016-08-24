d84e4917fd9d840c26d09c5e46f58b61ea82ea70
Diogo Ribeiro de Oliveira <diogoro.dba@gmail.com>
2016-08-23 23:49:55 -0300

    Adjusting the error caused by migrate tool.

--- JSONHelper/Extensions/NSDecimalNumber.swift
+++ JSONHelper/Extensions/NSDecimalNumber.swift
@@ -10,13 +10,13 @@ extension NSDecimalNumber: Convertible {
     guard let value = value else { return nil }
 
     if let doubleValue = value as? Double {
-      return self.`init`(value:)(double: doubleValue)
+        return self.init(value: doubleValue)
     } else if let stringValue = value as? String {
       return self.init(string: stringValue)
     } else if let floatValue = value as? Float {
-      return self.`init`(value:)(float: floatValue)
+      return self.init(value:floatValue)
     } else if let intValue = value as? Int {
-      return self.`init`(value:)(integer: intValue)
+      return self.init(value:intValue)
     }
 
     throw ConversionError.unsupportedType
